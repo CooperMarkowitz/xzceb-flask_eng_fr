0628ba758197a6e0273682ef66b2845650fdf61d
CooperMarkowitz <82242541+CooperMarkowitz@users.noreply.github.com>
2023-02-06 02:20:26 -0500

    Fixed tests

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -3,13 +3,13 @@ from translator import english_to_french, french_to_english
 
 class TestMyTranslator(unittest.TestCase):
     def test_frenchToEnglishNull(self):
-        self.assertIsNone(french_to_english(None))
-    def test_englishToFrenchNull(self):
-        self.assertIsNone(english_to_french(None))
-    def test_englishToFrenchWord(self):
-        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        self.assertNotEqual(french_to_english('None'), '   ') 
     def test_frenchToEnglishWord(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
+    def test_englishToFrenchNull(self):
+        self.assertNotEqual(english_to_french('None'), '   ') 
+    def test_englishToFrenchWord(self):
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
 
 if __name__=='__main__':
 	unittest.main()
